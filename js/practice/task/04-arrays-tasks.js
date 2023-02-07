a6ccfd085997c84322e622d66687ba017c678cdc
Nikolay <nikolay.kovzik@gmail.com>
2023-02-07 14:09:10 +0300

    getMovingSum

--- js/practice/task/04-arrays-tasks.js
+++ js/practice/task/04-arrays-tasks.js
@@ -234,7 +234,12 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   throw new Error('Not implemented');
+   const res = [];
+   arr.reduce((accum, item) => {
+      res.push(accum + item);
+      return accum + item;
+   }, 0)
+   return res
 }
 
 /**
