47c5b02a12e1f361655d886fd047b252111f8fde
Nikolay <nikolay.kovzik@gmail.com>
2023-02-08 01:52:27 +0300

    getRegexForSSN

--- js/practice/task/05-regex-tasks.js
+++ js/practice/task/05-regex-tasks.js
@@ -85,7 +85,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-  throw new Error('Not implemented');
+  return /((^(?!000)[0-9]{3}-)(?!00)[0-9]{2}-)(?!0000)[0-9]{4}/;
 }
 
 /**
