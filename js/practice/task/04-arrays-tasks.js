c571a96ce86d191c2bb61d57440607d2a2726341
Nikolay <nikolay.kovzik@gmail.com>
2023-02-07 16:12:00 +0300

    propagateItemsByPositionIndex

--- js/practice/task/04-arrays-tasks.js
+++ js/practice/task/04-arrays-tasks.js
@@ -273,7 +273,9 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-   throw new Error('Not implemented');
+   return arr.reduce((res, item, index) =>
+      [...res, ...Array(index + 1).fill(item)]
+      , [])
 }
 
 
