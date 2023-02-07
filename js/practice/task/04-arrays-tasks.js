e4fbdc42db5a63b81319d52a96e282a620b5a455
Nikolay <nikolay.kovzik@gmail.com>
2023-02-07 17:52:18 +0300

    getElementByIndexes

--- js/practice/task/04-arrays-tasks.js
+++ js/practice/task/04-arrays-tasks.js
@@ -555,7 +555,7 @@ function selectMany(arr, childrenSelector) {
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
 function getElementByIndexes(arr, indexes) {
-   throw new Error('Not implemented');
+   return indexes.reduce((res, item) => res[item], arr);
 }
 
 
