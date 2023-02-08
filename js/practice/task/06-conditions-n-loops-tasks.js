eddeabb63214dda325515f8bbe403af8f77e2d56
Nikolay <nikolay.kovzik@gmail.com>
2023-02-08 16:49:32 +0300

    doRectanglesOverlap

--- js/practice/task/06-conditions-n-loops-tasks.js
+++ js/practice/task/06-conditions-n-loops-tasks.js
@@ -132,7 +132,10 @@ function isTriangle(a, b, c) {
  *
  */
 function doRectanglesOverlap(rect1, rect2) {
-  throw new Error('Not implemented');
+  if ((rect2.top > rect1.top + rect1.height) || (rect2.left > rect1.left + rect1.width)) {
+    return false;
+  }
+  return true;
 }
 
 /**
